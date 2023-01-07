9e0502f9dac028c6c42a23f73a42b905e145d73f
Pavel Vaynerman <pv@etersoft.ru>
2023-01-07 05:03:07 +0300

    update for conan 1.56.0 and omniORB 4.2.4

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,10 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conan.tools.gnu import AutotoolsToolchain, Autotools
 import os
 
 class OmniorbConan(ConanFile):
     name = "omniorb"
-    version = "4.2.2"
+    version = "4.2.4"
     license = "GNU Lesser General Public License (for the libraries), and GNU General Public License (for the tools)"
     url = "<Package recipe repository url here, for issues about the package>"
     description = "omniORB is a robust high performance CORBA ORB for C++ and Python"
@@ -14,22 +15,23 @@ class OmniorbConan(ConanFile):
     root = "omniORB-" + version
     install_dir = 'omniorb-install'
 
+    def generate(self):
+        at_toolchain = AutotoolsToolchain(self, prefix="/usr")
+        at_toolchain.generate()
+
     def source(self):
         archive_name = "omniORB-%s.tar.bz2" % self.version
         source_url = "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-%s/%s" % ( self.version, archive_name )
         tools.get(source_url)
 
-    def build_configure(self):
+    def build(self):
         prefix = os.path.abspath(self.install_dir)
         with tools.chdir(self.root):
             env_build = AutoToolsBuildEnvironment(self)
             args = ['--prefix=%s' % prefix]
             env_build.configure(args=args)
             env_build.make()
-            env_build.make(args=['install'])
-
-    def build(self):
-        self.build_configure()
+            # env_build.make(args=['install'])
 
     def package(self):
         inc_dir = os.path.join(self.install_dir, 'include')
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,6 +7,7 @@ class OmniorbTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = "1"
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
         cmake.configure()
         cmake.build()
